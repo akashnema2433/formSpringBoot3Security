5cf6d1c09cb5259be2be33a835e3b3d223cef53d
akashnema <akashnema2023@gmail.com>
2024-04-19 17:21:37 +0530

    this is last commit upcomming commit not will be accepted

--- src/main/java/com/orgtest/repositories/UserRepository.java
+++ src/main/java/com/orgtest/repositories/UserRepository.java
@@ -2,8 +2,12 @@ package com.orgtest.repositories;
 
 import com.orgtest.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<User,Long> {
 
     User findByEmail(String email);
+
 }
